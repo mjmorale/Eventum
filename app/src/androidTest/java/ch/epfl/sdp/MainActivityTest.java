55209cab7e77d389e84a800682303dfe92ad2b8e
Michel <michel.morales@epfl.ch>
2020-02-27 16:03:30 +0100

    actually finished task c

---
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -0,0 +1,27 @@
+package ch.epfl.sdp;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testCanGreetUsers() {
+//        onView(withId(R.id.mainName)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
+//        onView(withId(R.id.mainGoButton)).perform(click());
+        // onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
+    }
+}
