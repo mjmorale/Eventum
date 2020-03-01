06871b3cab8d56b89392ed96f9077189ea9395bd
Corentin <corentin.bugnot@epfl.ch>
2020-03-01 22:10:27 +0100

    Add empty test to launch an Activity

---
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -0,0 +1,18 @@
+package ch.epfl.sdp;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testCanGreetUsers() {
+    }
+}
