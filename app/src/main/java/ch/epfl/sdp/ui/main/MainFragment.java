c7097bfa63a36bb0d2f99863672ce5b2ca9d6100
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-03-02 16:29:04 +0100

    Added toSwipe button in MainFragment

--- app/src/main/java/ch/epfl/sdp/ui/main/MainFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/main/MainFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 import ch.epfl.sdp.R;
+import ch.epfl.sdp.ui.swipe.SwipeFragment;
 
 public class MainFragment extends Fragment {
 
@@ -22,7 +26,18 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        Button swipeButton = (Button) view.findViewById(R.id.swipeButton);
+        swipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.container, new SwipeFragment());
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 
     @Override
