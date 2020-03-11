7e835dabddcca4b610d24f9437c12a8e51eb12f0
Karim <karim.sabaa@epfl.ch>
2020-03-11 17:24:06 +0100

    Added Google location button implemented the logic of the button: got the location from the sensor our self

--- app/src/main/java/ch/epfl/sdp/ui/main/MapFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/main/MapFragment.java
@@ -3,25 +3,37 @@ package ch.epfl.sdp.ui.main;
 
 import android.location.Location;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import ch.epfl.sdp.LocationHelper;
 import ch.epfl.sdp.R;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+    private MapView mapView;
+    private GoogleMap map;
+    private LocationHelper locationHelper;
+    private Location myLocation;
+    private boolean firstView;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locationHelper = new LocationHelper(getContext());
+        firstView=true;
     }
 
     @Override
@@ -30,27 +42,71 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.map_fragment, container, false);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapView= (MapView) view.findViewById(R.id.mapView2);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
         return view;
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
+    public void onMapReady(GoogleMap googlemap) {
+
         float zoom = 12;
-        LatLng Vidy = new LatLng(46.518615, 6.591796);
         LatLng Satellite = new LatLng(46.520564, 6.567827);
 
-        map.addMarker(new MarkerOptions().position(Vidy).title("Event A"));
-        map.addMarker(new MarkerOptions().position(Satellite).title("Event B"));
-
-        LocationHelper locationHelper = new LocationHelper(getContext());
         locationHelper.startListeningUserLocation(new LocationHelper.MyLocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                LatLng myLocation=new LatLng(location.getLatitude(), location.getLongitude());
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, zoom));
+                myLocation= location;
+                if(firstView){
+                    LatLng currentLocation = new LatLng(myLocation.getLatitude(),myLocation.getLongitude());
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,zoom));
+                    firstView=false;
+                }
+            }
+        });
+
+        map = googlemap;
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Satellite,zoom));
+        map.addMarker(new MarkerOptions().position(Satellite).title("Event B"));
+        map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                if(myLocation!=null){
+                    LatLng currentLocation = new LatLng(myLocation.getLatitude(),myLocation.getLongitude());
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,12));
+                }
+                return true;
             }
         });
     }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 }
