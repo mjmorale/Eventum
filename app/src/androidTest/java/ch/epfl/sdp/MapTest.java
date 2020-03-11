97cd4e32c7eaa41b9f37116d22b8ad0243d16cac
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-03-11 22:47:33 +0100

    first MapFragment test added

---
+++ app/src/androidTest/java/ch/epfl/sdp/MapTest.java
@@ -0,0 +1,34 @@
+package ch.epfl.sdp;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MapTest{
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Before
+    public void setup(){
+        onView(withText("Map")).perform(click());
+    }
+
+    @Test
+    public void checkThatMapIsDisplayed() {
+        onView(withId(R.id.mapView2)).check(matches((isDisplayed())));
+    }
+}
