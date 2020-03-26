f34923cb40691cecb3d7defbd81f5a5a92204394
Karim <karim.sabaa@epfl.ch>
2020-03-26 22:56:20 +0100

    pull request adjustment

--- app/src/main/java/ch/epfl/sdp/ui/event/EventFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/event/EventFragment.java
@@ -1,45 +1,25 @@
 package ch.epfl.sdp.ui.event;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModelProvider;
-
-
-import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Picture;
 import android.graphics.drawable.BitmapDrawable;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.facebook.FacebookSdk;
-import ch.epfl.sdp.Event;
-import ch.epfl.sdp.R;
-import ch.epfl.sdp.databinding.EventFragmentBinding;
-import ch.epfl.sdp.db.Database;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.facebook.share.internal.MessageDialogFeature;
 import com.facebook.share.model.ShareContent;
 import com.facebook.share.model.ShareHashtag;
-import com.facebook.share.model.ShareLinkContent;
-
 import com.facebook.share.model.ShareMediaContent;
 import com.facebook.share.model.SharePhoto;
-import com.facebook.share.widget.MessageDialog;
 import com.facebook.share.widget.ShareDialog;
-import com.google.android.gms.maps.GoogleMap;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.util.List;
+import ch.epfl.sdp.databinding.EventFragmentBinding;
+import ch.epfl.sdp.db.Database;
 
 
 public class EventFragment extends Fragment  implements View.OnClickListener {
@@ -100,9 +80,7 @@ public class EventFragment extends Fragment  implements View.OnClickListener {
 
     @Override
     public void onClick(View view){
-        if(mSharedDialog.canShow(mShareContent)) {
             mSharedDialog.show(mShareContent, ShareDialog.Mode.AUTOMATIC);
-        }
     }
 
     @Override
--- app/src/main/java/ch/epfl/sdp/ui/eventdetail/EventDetailFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/eventdetail/EventDetailFragment.java
@@ -1,7 +1,5 @@
 package ch.epfl.sdp.ui.eventdetail;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,23 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.facebook.share.model.ShareContent;
-import com.facebook.share.model.ShareHashtag;
-import com.facebook.share.model.ShareMediaContent;
-import com.facebook.share.model.SharePhoto;
-import com.facebook.share.widget.ShareDialog;
-
 import ch.epfl.sdp.Event;
 import ch.epfl.sdp.databinding.EventFragmentBinding;
-
 import ch.epfl.sdp.ui.swipe.SwipeFragment;
-public class EventDetailFragment extends Fragment implements View.OnClickListener {
+public class EventDetailFragment extends Fragment{
     private EventFragmentBinding mBinding;
     private Event mEvent;
     private SwipeFragment mSwipeFragment;
 
-    private ShareContent mShareContent;
-    private ShareDialog mSharedDialog;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -38,13 +27,6 @@ public class EventDetailFragment extends Fragment implements View.OnClickListene
         this.mEvent = event;
         this.mSwipeFragment = swipeFragment;
 
-    }
-    @Override
-    public void onClick(View view){
-        if(view.getId()==mBinding.sharingButton.getId())
-            if(mSharedDialog.canShow(mShareContent)) {
-                mSharedDialog.show(mShareContent, ShareDialog.Mode.AUTOMATIC);
-            }
     }
     @Nullable
     @Override
@@ -62,22 +44,7 @@ public class EventDetailFragment extends Fragment implements View.OnClickListene
                 getActivity().getSupportFragmentManager().beginTransaction().replace(thisFragment.getId(), mSwipeFragment).commit();
             }
         });
-        prepareShare();
         return mBinding.getRoot();
-    }
-
-    private void prepareShare(){
-        mSharedDialog = new ShareDialog(this);
-        BitmapDrawable bitmapDrawable = ((BitmapDrawable) mBinding.imageView.getDrawable());
-        Bitmap bitmap = bitmapDrawable.getBitmap();
-        mShareContent = new ShareMediaContent.Builder()
-                .setShareHashtag(
-                        new ShareHashtag.Builder().setHashtag(
-                                mBinding.title.getText().toString() + " :\n\n " +
-                                        mBinding.description.getText().toString()).build())
-                .addMedium(new SharePhoto.Builder().setBitmap(bitmap).build())
-                .build();
-        mBinding.sharingButton.setOnClickListener(this);
     }
 
 }
