c44551f752f160ffd3a80d7dd37a86537750d4c4
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-03-31 15:10:56 +0200

    rework of the map basic version (without current location)

--- app/src/main/java/ch/epfl/sdp/ui/main/map/MapFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/main/map/MapFragment.java
@@ -6,62 +6,113 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import java.util.List;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.firebase.firestore.FirebaseFirestore;
 import ch.epfl.sdp.Event;
 import ch.epfl.sdp.R;
+import ch.epfl.sdp.databinding.FragmentMapBinding;
 import ch.epfl.sdp.db.Database;
-import ch.epfl.sdp.platforms.google.map.GoogleMapProvider;
+import ch.epfl.sdp.platforms.firebase.db.FirestoreDatabase;
 
-public class MapFragment extends Fragment{
+// This is an example of a minimal implementation of a Fragment.
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapViewModel mViewModel;
+    private final MapViewModel.MapViewModelFactory mFactory;
+    private FragmentMapBinding mBinding;
     private MapView mMapView;
-    private GoogleMapProvider mGoogleMapProvider;
-    private Database mDataBase;
-    private LiveData<List<Event>> mEvents;
 
-    public MapFragment(Database db) {
-        mDataBase = db;
+    // Replace value1 and value2 by your own arguments
+    // If the fragment does not require arguments through a Bundle,
+    // this method can be removed
+//    public static MapFragment getInstance(String value1, String value2) {
+//        Bundle bundle = new Bundle();
+//        //TODO: replace with your own arguments
+//        bundle.putString("value1", value1);
+//        bundle.putString("value2", value2);
+//
+//        MapFragment fragment = new MapFragment();
+//        fragment.setArguments(bundle);
+//
+//        return fragment;
+//    }
+
+    // The default constructor is required by Android.
+    // E.g., if the fragment is recreated, the default constructor is used.
+    // This means that the fragment cannot rely on parameters passed through the constructor
+    public MapFragment() {
+        mFactory = new MapViewModel.MapViewModelFactory();
+        mFactory.setDatabase(new FirestoreDatabase(FirebaseFirestore.getInstance()));
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mEvents = mDataBase.query("events").liveData(Event.class);
+    // This constructor should only be used in tests through a custom fragment factory
+    // (for the same reason as mentionned before).
+    // You can pass any number of arguments you want and set them manually in the view model factory.
+    @VisibleForTesting
+    public MapFragment(@NonNull Database database) {
+        mFactory = new MapViewModel.MapViewModelFactory();
+        mFactory.setDatabase(database);
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        // Initializes the view using the view bindings created for your fragment.
+        mBinding = FragmentMapBinding.inflate(inflater, container, false);
 
-        mMapView= view.findViewById(R.id.mapView);
+        mMapView = mBinding.getRoot().findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
 
-        mGoogleMapProvider = new GoogleMapProvider(this ,mMapView);
-        mGoogleMapProvider.setMyLocationEnabled(true);
-        
-        mEvents.observe(getViewLifecycleOwner(), event -> {
-            for(Event e: event){
-                addMarker(e.getTitle(),e.getLocation(), mGoogleMapProvider);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // If your fragment takes arguments using a Bundle and that the view model requires this value,
+        // your can set it in the factory here.
+        // If your fragment does not require a Bundle, then you can delete this part
+//        Bundle args = getArguments();
+//        if(args != null) {
+//            //TODO: replace with your own arguments
+//            mFactory.setValue1(args.getString("value1"));
+//        }
+
+        // This is where the view model factory is called to create a new view model.
+        // Make sure your factory is fully initialized before this point.
+        mViewModel = new ViewModelProvider(this, mFactory).get(MapViewModel.class);
+
+        mViewModel.getEvents().observe(getViewLifecycleOwner(), events -> {
+            for(Event event: events){
+                mViewModel.addMarker(event);
             }
         });
 
-        return view;
     }
 
-    public void addMarker(String eventName, LatLng coordinates, GoogleMapProvider googleMapProvider) {
-        googleMapProvider.addMarker(new MarkerOptions().position(coordinates).title(eventName));
+    // Forces the deletion of the view binding instance when the view is deleted.
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mViewModel.setMap(googleMap);
     }
 
     @Override
     public void onResume() {
-        super.onResume();
         mMapView.onResume();
+        super.onResume();
     }
 
     @Override
--- app/src/main/java/ch/epfl/sdp/ui/main/map/MapViewModel.java
+++ app/src/main/java/ch/epfl/sdp/ui/main/map/MapViewModel.java
@@ -1,4 +1,64 @@
 package ch.epfl.sdp.ui.main.map;
 
-public class MapViewModel {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.List;
+import ch.epfl.sdp.Event;
+import ch.epfl.sdp.db.Database;
+import ch.epfl.sdp.ui.ParameterizedViewModelFactory;
+
+import static ch.epfl.sdp.ObjectUtils.verifyNotNull;
+
+// This is an example of a minimal implementation of a ViewModel.
+public class MapViewModel extends ViewModel {
+
+    static class MapViewModelFactory extends ParameterizedViewModelFactory {
+
+        MapViewModelFactory() {
+            super(Database.class);
+        }
+
+        void setDatabase(@NonNull Database database) {
+            setValue(0, verifyNotNull(database));
+        }
+    }
+
+    private LiveData<List<Event>> mEventsLive;
+    private final Database mDatabase;
+    private GoogleMap mMap;
+    private LiveData<GoogleMap> mMapLive;
+
+    // This constructor can be used altought it should not be used in the actual application.
+    // Use a view model factory instead.
+    //TODO: replace with your own signature.
+    public MapViewModel(@NonNull Database database) {
+        mDatabase = database;
+        mEventsLive = mDatabase.query("events").liveData(Event.class);
+    }
+
+    public LiveData<List<Event>> getEvents() {
+        return mEventsLive;
+    }
+
+    public void addMarker(Event event) {
+        if (mMap!=null) {
+            LatLng coordinates = event.getLocation();
+            String name = event.getTitle();
+            Marker marker = mMap.addMarker(new MarkerOptions().position(coordinates).title(name));
+        }
+    }
+
+    public GoogleMap getMap() {
+        return mMap;
+    }
+
+    public void setMap(GoogleMap map) {
+        mMap = map;
+    }
+
 }
