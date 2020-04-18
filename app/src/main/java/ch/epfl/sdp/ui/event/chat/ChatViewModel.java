604b2f105c41ccbadef4de4b613b2207dacccca6
dmizr <david.mizrahi@epfl.ch>
2020-04-18 19:22:35 +0200

    add ChatMessage class and database builder

--- app/src/main/java/ch/epfl/sdp/ui/event/chat/ChatViewModel.java
+++ app/src/main/java/ch/epfl/sdp/ui/event/chat/ChatViewModel.java
@@ -2,9 +2,17 @@ package ch.epfl.sdp.ui.event.chat;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
+import java.util.Date;
+
+import ch.epfl.sdp.ChatMessage;
+import ch.epfl.sdp.User;
 import ch.epfl.sdp.db.Database;
+import ch.epfl.sdp.db.queries.CollectionQuery;
 import ch.epfl.sdp.ui.ParameterizedViewModelFactory;
+import ch.epfl.sdp.ui.auth.AuthViewModel;
+
 
 import static ch.epfl.sdp.ObjectUtils.verifyNotNull;
 
@@ -12,8 +20,7 @@ public class ChatViewModel extends ViewModel {
 
     static class ChatViewModelFactory extends ParameterizedViewModelFactory {
 
-        // The constructor describes the types of the parameters that will eventually be
-        // passed to the view model's constructor, in our case two strings.
+
         ChatViewModelFactory() {
             super(Database.class, String.class);
         }
@@ -27,5 +34,35 @@ public class ChatViewModel extends ViewModel {
         }
     }
 
-    // TODO: Implement the ViewModel
+    interface OnMessageAddedCallback {
+        void onSuccess(String messageRef);
+        void onFailure(Exception exception);
+    }
+
+    private final CollectionQuery mEventCollection;
+    private final Database mDatabase;
+    private final String mEventRef;
+    private User mUser;
+
+    public ChatViewModel(@NonNull Database database, @NonNull String eventRef) {
+        verifyNotNull(database, eventRef);
+        mDatabase = database;
+        mEventRef = eventRef;
+        mEventCollection = database.query("events").document(eventRef).collection("messages");
+
+        //mUser = new ViewModelProvider(this, new AuthViewModel.AuthViewModelFactory()).get(AuthViewModel.class).getUser();
+
+    }
+
+    public void addMessage(@NonNull String message, @NonNull OnMessageAddedCallback callback) {
+
+        ChatMessage chatMessage = new ChatMessage(message, new Date(), "a", "b");
+        mEventCollection.create(chatMessage, res -> {
+            if(res.isSuccessful()) {
+                callback.onSuccess(res.getData());
+            } else {
+                callback.onFailure(res.getException());
+            }
+        });
+    }
 }
