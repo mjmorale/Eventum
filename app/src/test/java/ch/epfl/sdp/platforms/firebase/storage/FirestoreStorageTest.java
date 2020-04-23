134a78b18f3477b1bee33e402fc4a2748e0bd444
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-04-23 17:48:05 +0200

    test in FirestoreStorageTest added

--- app/src/test/java/ch/epfl/sdp/platforms/firebase/storage/FirestoreStorageTest.java
+++ app/src/test/java/ch/epfl/sdp/platforms/firebase/storage/FirestoreStorageTest.java
@@ -1,6 +1,11 @@
 package ch.epfl.sdp.platforms.firebase.storage;
 
+import android.net.Uri;
+
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,12 +14,31 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import ch.epfl.sdp.platforms.firebase.db.FirestoreDatabase;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class FirestoreStorageTest {
     @Mock
     private FirebaseStorage mStorage;
+
+    @Mock
+    private Uri mUri;
+
+    @Mock
+    private FirestoreStorage.UrlReadyCallback mCallback;
+
+    @Mock
+    private UploadTask mTask;
+
+    @Mock
+    private StorageReference mRef;
+
+    @Mock
+    private StorageTask mStTask;
 
     @Before
     public void setup() {
@@ -26,9 +50,25 @@ public class FirestoreStorageTest {
         FirestoreStorage storage = new FirestoreStorage(null);
     }
 
+    @Test
+    public void FirestoreStorage_ListenersAddedOnUploadImage() {
+        FirestoreStorage storage = new FirestoreStorage(mStorage);
 
+        FirestoreStorage.UrlReadyCallback callback = new FirestoreStorage.UrlReadyCallback() {
+            @Override
+            public void onSuccess(String url) { }
 
+            @Override
+            public void onFailure() { }
+        };
 
+        when(mStorage.getReference(anyString())).thenReturn(mRef);
+        when(mRef.putFile(mUri)).thenReturn(mTask);
+        when(mTask.addOnSuccessListener(any())).thenReturn(mStTask);
 
+        storage.uploadImage(mUri, callback);
 
+        verify(mTask).addOnSuccessListener(any());
+        verify(mStTask).addOnFailureListener(any());
+    }
 }
