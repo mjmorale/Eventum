f6471dc4310867b5ca7d2d1b523bb8f6579e75b7
dmizr <david.mizrahi@epfl.ch>
2020-04-23 18:50:35 +0200

    back button on chat goes back to event

--- app/src/main/java/ch/epfl/sdp/ui/event/DefaultEventFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/event/DefaultEventFragment.java
@@ -82,7 +82,7 @@ public class DefaultEventFragment extends Fragment{
             mBinding.address.setText(event.getAddress());
         });
         mBinding.chatButton.setOnClickListener(v->{
-            getActivity().getSupportFragmentManager().beginTransaction().replace(this.getId(), ChatFragment.getInstance(mViewModel.getEventRef())).commit();
+            getActivity().getSupportFragmentManager().beginTransaction().replace(this.getId(), ChatFragment.getInstance(mViewModel.getEventRef())).addToBackStack(null).commit();
         });
     }
 
