d1b20673e38ea85d07c2b6107fd08872ee815fbd
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-04-27 13:05:28 +0200

    javadoc for LocationService

--- app/src/main/java/ch/epfl/sdp/map/LocationService.java
+++ app/src/main/java/ch/epfl/sdp/map/LocationService.java
@@ -3,8 +3,22 @@ package ch.epfl.sdp.map;
 import android.content.Context;
 import android.location.Location;
 
+/**
+ * Location service for giving the last known location of the android device
+ */
 public interface LocationService {
+    /**
+     * Method to get the last known location
+     *
+     * @param context the environment the application is currently running in
+     * @return the last know location of the android device
+     */
     Location getLastKnownLocation(Context context);
 
+    /**
+     * Method to get the current state of location
+     *
+     * @return the current enabled/disabled state of location
+     */
     boolean isLocationEnabled();
 }
