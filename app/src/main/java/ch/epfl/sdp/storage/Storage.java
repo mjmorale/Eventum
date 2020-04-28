e7a0f30570bd1a6ccaaeb2a186cf2a53e800dcb9
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-04-28 10:36:06 +0200

    javadoc for Storage

--- app/src/main/java/ch/epfl/sdp/storage/Storage.java
+++ app/src/main/java/ch/epfl/sdp/storage/Storage.java
@@ -10,5 +10,11 @@ import ch.epfl.sdp.platforms.firebase.storage.FirestoreStorage;
  * To store an image on firebase storage from an image uri.
  */
 public interface Storage {
+    /**
+     * Method to upload an image on the storage
+     *
+     * @param imageUri of the image to be uploaded
+     * @param callback called when the upload is finished
+     */
     void uploadImage(@NonNull Uri imageUri, @NonNull FirestoreStorage.UrlReadyCallback callback);
 }
