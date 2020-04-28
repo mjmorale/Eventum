79203f3329868f1a203c29d9cbd3ffe434ace097
Corentin <corentin.bugnot@epfl.ch>
2020-04-28 19:01:54 +0200

    Javadoc for database interfaces

--- app/src/main/java/ch/epfl/sdp/db/queries/CollectionQuery.java
+++ app/src/main/java/ch/epfl/sdp/db/queries/CollectionQuery.java
@@ -10,22 +10,72 @@ import com.google.firebase.firestore.GeoPoint;
 
 import org.imperiumlabs.geofirestore.GeoFirestore.CompletionCallback;
 
+/**
+ * A Collection is a list of documents {@link ch.epfl.sdp.db.queries.DocumentQuery} this is
+ * the interface that each Collection should implement
+ */
 public interface CollectionQuery extends Query {
 
+    /**
+     * To query a document contained inside this collection.
+     *
+     * @param document reference to the desired document
+     * @return query {@link ch.epfl.sdp.db.queries.DocumentQuery} object associated
+     * to the reference argument
+     */
     DocumentQuery document(@NonNull String document);
 
+    /**
+     * {@link ch.epfl.sdp.db.queries.FilterQuery#whereFieldEqualTo(String, Object)}
+     */
     FilterQuery whereFieldEqualTo(@NonNull String field, Object value);
 
+    /**
+     * {@link ch.epfl.sdp.db.queries.FilterQuery#orderBy(String)}
+     */
     FilterQuery orderBy(@NonNull String field);
 
+
+    /**
+     * {@link ch.epfl.sdp.db.queries.FilterQuery#limitCount(int)}
+     */
     FilterQuery limitCount(int count);
 
+    /**
+     * Query that returns filtered events based on a radius centered at a location position.
+     *
+     * @param location target location
+     * @param radius target radius
+     * @return query {@link ch.epfl.sdp.db.queries.LocationQuery} object associated to the filtered
+     * query
+     */
     LocationQuery atLocation(GeoPoint location, double radius);
 
+    /**
+     * Get the object associated to this query.
+     *
+     * @param type class type that should be returned
+     * @param callback class that will be called once the result is returned
+     * @param <T> type that should be returned
+     */
     <T> void get(@NonNull Class<T> type, @NonNull OnQueryCompleteCallback<List<T>> callback);
 
+    /**
+     * Get the livedata the object associated to this query.
+     *
+     * @param type class type that should be returned
+     * @param <T> type that should be returned
+     */
     <T> LiveData<List<T>> liveData(@NonNull Class<T> type);
 
+    /**
+     * Create an object inside the collection associated to this query.
+     *
+     * @param object class that should put inside
+     * @param callback class that will be called once the result is returned and return the
+     *                 reference of the created object
+     * @param <T> type that should be created
+     */
     <T> void create(@NonNull T object, @NonNull OnQueryCompleteCallback<String> callback);
 
 }
--- app/src/main/java/ch/epfl/sdp/db/queries/DocumentQuery.java
+++ app/src/main/java/ch/epfl/sdp/db/queries/DocumentQuery.java
@@ -5,19 +5,67 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import ch.epfl.sdp.db.DatabaseObjectBuilder;
 
+/**
+ * A document is contained inside a collection {@link ch.epfl.sdp.db.queries.CollectionQuery} this is
+ * the interface that each Document should implement
+ */
 public interface DocumentQuery extends Query {
 
+    /**
+     * To query the parent collection.
+     *
+     * @param collection reference to the parent collection
+     * @return query {@link ch.epfl.sdp.db.queries.CollectionQuery} object associated
+     * to the reference argument
+     */
     CollectionQuery collection(String collection);
 
+
+    /**
+     * Check if this query is associated to an existing document.
+     *
+     * @param callback class that will be called once the result is returned
+     */
     void exists(@NonNull OnQueryCompleteCallback<Boolean> callback);
 
+    /**
+     * Get the object associated to this query.
+     *
+     * @param type class type that should be returned
+     * @param callback class that will be called once the result is returned
+     * @param <T> type that should be returned
+     */
     <T> void get(@NonNull Class<T> type, @NonNull OnQueryCompleteCallback<T> callback);
 
+    /**
+     * Set or replace the object associated to this query.
+     *
+     * @param callback class that will be called once created
+     * @param <T> type that should be returned
+     */
     <T> void set(@NonNull T object, @NonNull OnQueryCompleteCallback<Void> callback);
 
+    /**
+     * Update a field of the object associated to this query.
+     *
+     * @param field target field to update
+     * @param value object that should replace the targeted field
+     * @param callback class that will be called once updated
+     */
     void update(@NonNull String field, @NonNull Object value, @NonNull OnQueryCompleteCallback<Void> callback);
 
+    /**
+     * Get the livedata the object associated to this query.
+     *
+     * @param type class type that should be returned
+     * @param <T> type that should be returned
+     */
     <T> LiveData<T> livedata(@NonNull Class<T> type);
 
+    /**
+     * Delete the object associated to this query.
+     *
+     * @param callback class that will be called once deleted
+     */
     void delete(@NonNull OnQueryCompleteCallback<Void> callback);
 }
