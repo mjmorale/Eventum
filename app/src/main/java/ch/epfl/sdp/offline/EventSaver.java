e6872fa7aa3c001f0eb9182d1c5bde8da1b8aa6e
Martin <beaussart.martin@protonmail.ch>
2020-04-28 20:22:16 +0200

    implementation Event cache

--- app/src/main/java/ch/epfl/sdp/offline/EventSaver.java
+++ app/src/main/java/ch/epfl/sdp/offline/EventSaver.java
@@ -1,16 +1,51 @@
 package ch.epfl.sdp.offline;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class EventSaver <Event> extends ObjectSaver {
 
+    static private HashMap<String, Date> statusFiles = new HashMap<String, Date>();
+
+    @NotNull
     @Override
     protected String getCollectionString() {
         return "Events";
     }
 
-    public List<Event> getAllEvents(){
+    /**
+     * Saves an object in a file
+     * @param deleteDate When we can delete the temp file
+     * @param toSave The object to save in the collection
+     * @param docReference Id of the document
+     */
+    public void saveFile(Event toSave, String docReference, Date deleteDate) throws IOException {
+        super.saveFile((Serializable) toSave,docReference,deleteDate);
 
-        return null;
+        //update status
+        statusFiles.put(docReference, deleteDate);
+
+        //remove old files
+        for (String key: statusFiles.keySet()) {
+            if (Objects.requireNonNull(statusFiles.get(key)).before(new Date())){
+                //delete the file and remove from status
+                File outDatedFile = File.createTempFile(docReference, null);
+                outDatedFile.delete();
+                statusFiles.remove(key);
+            }
+        }
+    }
+
+    public List<Event> getAllEvents() throws IOException, ClassNotFoundException {
+        List<String> listReference = new ArrayList<String>(statusFiles.keySet());
+        return getMultipleFile(listReference);
     }
 }
--- app/src/main/java/ch/epfl/sdp/offline/ObjectSaver.java
+++ app/src/main/java/ch/epfl/sdp/offline/ObjectSaver.java
@@ -6,14 +6,17 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Classes implementing this abstract class allows to save an Object in the app-specific storage
@@ -21,13 +24,13 @@ import java.util.List;
  */
 public abstract class ObjectSaver <T extends Serializable> implements OfflineDatabaseSaver {
 
-    private Dictionary<String, Date> statusFiles = new Hashtable<String, Date>();
+    static private HashMap<String, Date> statusFiles = new HashMap<String, Date>();
 
     /**
      * Saves an object in a file
      * @param deleteDate When we can delete the temp file
      * @param toSave The object to save in the collection
-     * @param docReference
+     * @param docReference Id of the document
      */
     public void saveFile(T toSave, String docReference, Date deleteDate) throws IOException {
         File newFile = File.createTempFile(docReference, null);
@@ -43,31 +46,43 @@ public abstract class ObjectSaver <T extends Serializable> implements OfflineDat
         o.writeObject(toSave);
         o.close();
         f.close();
-
-        //update status  of the cache
-        statusFiles.put(docReference, deleteDate);
-
-        //check all status of the cache
-
     }
 
     /**
-     *
-     * @param docReference
+     * @param listReference list of id from files you want to get
      * @return
      */
-    public List<T> getMultipleFile(List<String> docReference){
+    public List<T> getMultipleFile(List<String> listReference) throws IOException, ClassNotFoundException {
+        List<T> result = new ArrayList<T>();
 
-        return null;
+        for (String docReference : listReference) {
+            File fileDescriptor = File.createTempFile(docReference, null);
+            FileInputStream fi = new FileInputStream(fileDescriptor);
+            ObjectInputStream oi = new ObjectInputStream(fi);
+
+            T tempRead = (T) oi.readObject();
+            result.add(tempRead);
+
+            oi.close();
+            fi.close();
+        }
+        return result;
     }
 
     /**
-     *
-     * @param docReference
+     * @param docReference Id of the document
      * @return
      */
-    public T getSingleFile(String docReference){
-        return null;
+    public T getSingleFile(String docReference) throws IOException, ClassNotFoundException {
+        File fileDescriptor = File.createTempFile(docReference, null);
+        FileInputStream fi = new FileInputStream(fileDescriptor);
+        ObjectInputStream oi = new ObjectInputStream(fi);
+
+        T tempRead = (T) oi.readObject();
+        oi.close();
+        fi.close();
+
+        return tempRead;
     }
 
     @NonNull
