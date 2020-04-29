55f6322bd01b88fc91cb4213e0200b3f009051d2
Michel <michel.morales@epfl.ch>
2020-04-30 00:13:17 +0200

    failing commit

--- app/src/main/java/ch/epfl/sdp/platforms/firebase/db/queries/CachingCollectionQuery.java
+++ app/src/main/java/ch/epfl/sdp/platforms/firebase/db/queries/CachingCollectionQuery.java
@@ -3,6 +3,7 @@ package ch.epfl.sdp.platforms.firebase.db.queries;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 
 import java.util.List;
@@ -14,48 +15,53 @@ import ch.epfl.sdp.db.queries.LocationQuery;
 
 public class CachingCollectionQuery implements CollectionQuery {
     private final FirebaseCollectionQuery mCollectionQuery;
-
-    public CachingCollectionQuery(FirebaseCollectionQuery firebaseCollectionQuery){
-        mCollectionQuery = firebaseCollectionQuery;
+    private final String mCollection;
+    public CachingCollectionQuery(FirebaseFirestore mDb, String collection){
+        mCollectionQuery = new FirebaseCollectionQuery(mDb, mDb.collection(collection));
+        mCollection = collection;
     }
 
     @Override
     public DocumentQuery document(@NonNull String document) {
-        return null;
+        if(mCollection != "Events")
+            return mCollectionQuery.document(document);
+        else {
+            return null;
+        }
     }
 
     @Override
     public FilterQuery whereFieldEqualTo(@NonNull String field, Object value) {
-        return null;
+        return mCollectionQuery.whereFieldEqualTo(field, value);
     }
 
     @Override
     public FilterQuery orderBy(@NonNull String field) {
-        return null;
+        return mCollectionQuery.orderBy(field);
     }
 
     @Override
     public FilterQuery limitCount(int count) {
-        return null;
+        return mCollectionQuery.limitCount(count);
     }
 
     @Override
     public LocationQuery atLocation(GeoPoint location, double radius) {
-        return null;
+        return mCollectionQuery.atLocation(location, radius);
     }
 
     @Override
     public <T> void get(@NonNull Class<T> type, @NonNull OnQueryCompleteCallback<List<T>> callback) {
-
+        mCollectionQuery.get(type, callback);
     }
 
     @Override
     public <T> LiveData<List<T>> liveData(@NonNull Class<T> type) {
-        return null;
+        return mCollectionQuery.liveData(type);
     }
 
     @Override
     public <T> void create(@NonNull T object, @NonNull OnQueryCompleteCallback<String> callback) {
-
+        mCollectionQuery.create(object, callback);
     }
 }
