a3a6b6d938bc669269ff70b9c1d7a573fd55e9b3
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-05-02 16:34:58 +0200

    timepicker added for event creation

--- app/src/main/java/ch/epfl/sdp/ui/event/DefaultEventFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/event/DefaultEventFragment.java
@@ -104,7 +104,7 @@ public class DefaultEventFragment extends Fragment{
         mBinding.sharingButton.setOnClickListener(v->startActivity(mEventSharing.getShareIntent()));
 
         mViewModel.getEvent().observe(getViewLifecycleOwner(), event -> {
-            mBinding.date.setText(event.getDateStr());
+            mBinding.date.setText(event.getDate().toString());
             mBinding.description.setText(event.getDescription());
             mBinding.title.setText(event.getTitle());
             mBinding.address.setText(event.getAddress());
