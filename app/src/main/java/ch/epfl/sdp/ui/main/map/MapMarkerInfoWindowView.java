7d5e9a69822c22a195e09e76d85af609e4d8f50c
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-05-04 19:37:54 +0200

    MapMarkerInfoWindowView with event details and image (buggy)

--- app/src/main/java/ch/epfl/sdp/ui/main/map/MapMarkerInfoWindowView.java
+++ app/src/main/java/ch/epfl/sdp/ui/main/map/MapMarkerInfoWindowView.java
@@ -3,21 +3,30 @@ package ch.epfl.sdp.ui.main.map;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
 import ch.epfl.sdp.Event;
 import ch.epfl.sdp.R;
 
-// ADD JAVADOC !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+/**
+ * View for displaying an information window on a marker click on the map
+ */
 public class MapMarkerInfoWindowView implements GoogleMap.InfoWindowAdapter {
 
     private View mMarkerView;
     private MapViewModel mMapViewModel;
     private Context mContext;
 
+    /**
+     * @param mapViewModel the view model of the map where the markers are
+     * @param context the environment the application is currently running in
+     */
     public MapMarkerInfoWindowView(MapViewModel mapViewModel, Context context) {
 
         mMapViewModel = mapViewModel;
@@ -30,8 +39,14 @@ public class MapMarkerInfoWindowView implements GoogleMap.InfoWindowAdapter {
     @Override
     public View getInfoWindow(Marker marker) {
         Event event = mMapViewModel.getEventFromMarker(marker);
+        TextView title = mMarkerView.findViewById(R.id.map_event_title);
+        TextView date = mMarkerView.findViewById(R.id.map_event_date);
         TextView description = mMarkerView.findViewById(R.id.map_event_description);
+        ImageView image = mMarkerView.findViewById(R.id.map_event_image);
+        title.setText(event.getTitle());
         description.setText(event.getDescription());
+        date.setText(event.getDate().toString());
+        Glide.with(mContext).load(event.getImageId()).into(image);
 
         return mMarkerView;
     }
