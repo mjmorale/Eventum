1b4e03f70e45a452799cf838a0efe3eaa2e90d73
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-05-05 18:34:34 +0200

    first test for the MapMarkerInfoWindowView

--- app/src/test/java/ch/epfl/sdp/ui/main/map/MapMarkerInfoWindowViewTest.java
+++ app/src/test/java/ch/epfl/sdp/ui/main/map/MapMarkerInfoWindowViewTest.java
@@ -20,6 +20,45 @@ import static org.mockito.Mockito.when;
 
 public class MapMarkerInfoWindowViewTest {
 
+    @Mock
+    private Context mContext;
+
+    @Mock
+    private MapViewModel mMapViewModel;
+
+    @Mock
+    private LayoutInflater mInflater;
+
+    @Mock
+    private View mMarkerView;
+
+    @Mock
+    private Event mEvent;
+
+    @Mock
+    private TextView mTitleView;
+
+    @Mock
+    private TextView mDateView;
+
+    @Mock
+    private TextView mDescriptionView;
+
+    @Mock
+    private ImageView mImageView;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void MapMarkerInfoWindowView_ConstructorTest() {
+        when(mInflater.inflate(any(), any())).thenReturn(mMarkerView);
+        new MapMarkerInfoWindowView(mMapViewModel, mContext, mInflater);
+        verify(mInflater).inflate(R.layout.view_map_marker_info_window, null);
+    }
+
 
 
 
