eb8becf10be12d8cc5912b8690101d518c594b27
dmizr <david.mizrahi@epfl.ch>
2020-05-11 17:52:38 +0200

    remove synchronous url fetching

--- app/src/main/java/ch/epfl/sdp/platforms/openweathermap/OpenWeatherMapFetcher.java
+++ app/src/main/java/ch/epfl/sdp/platforms/openweathermap/OpenWeatherMapFetcher.java
@@ -6,21 +6,9 @@ import androidx.annotation.NonNull;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.model.LatLng;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import ch.epfl.sdp.weather.Weather;
 import ch.epfl.sdp.weather.WeatherFetcher;
 
@@ -49,39 +37,11 @@ public class OpenWeatherMapFetcher implements WeatherFetcher {
         StringBuilder sb;
         sb = new StringBuilder();
         sb.append("https://api.openweathermap.org/data/2.5/onecall?");
-        sb.append("lat=").append(location.latitude);
-        sb.append("&").append("lon=").append(location.longitude);
-        sb.append("&").append("&exclude=hourly");
-        sb.append("&").append("units=metric");
-        sb.append("&").append("appid=").append(apiK);
+        sb.append("lat=").append(location.latitude).append("&lon=").append(location.longitude);
+        sb.append("&exclude=hourly").append("&units=metric");
+        sb.append("&appid=").append(apiK);
 
         return sb.toString();
     }
-
-
-    private static String getDataFromURL(String urlString) throws IOException {
-
-        URL url = new URL(urlString);
-
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setReadTimeout(10000);
-        urlConnection.setConnectTimeout(15000);
-        urlConnection.connect();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        br.close();
-        urlConnection.disconnect();
-
-        return sb.toString();
-    }
-
 }
 
