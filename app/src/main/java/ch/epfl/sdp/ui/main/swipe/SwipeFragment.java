53a64093a9a383e8ccd3cf57e0408d71e9c664cf
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-05-11 12:30:01 +0200

    refactoring for less lines in onCreateView

--- app/src/main/java/ch/epfl/sdp/ui/main/swipe/SwipeFragment.java
+++ app/src/main/java/ch/epfl/sdp/ui/main/swipe/SwipeFragment.java
@@ -121,25 +121,7 @@ public class SwipeFragment extends Fragment implements SwipeFlingAdapterView.onF
                 mArrayAdapter.addAll(events);
                 mArrayAdapter.sort((o1, o2) -> o1.getObject().getDate().compareTo(o2.getObject().getDate()));
                 mNumberSwipe = 0;
-
-                // to have a event on top when clicking on it on the map
-               Bundle bundle = this.getArguments();
-               if (bundle != null) {
-                   float eventHash = bundle.getFloat("eventHash", DEFAULT_VALUE);
-                   if (eventHash != DEFAULT_VALUE) {
-                       DatabaseObject<Event> searchedDatabaseObject = null;
-                       for (int index = 0; index < mArrayAdapter.getCount() && searchedDatabaseObject == null; index++) {
-                           DatabaseObject<Event> databaseObject = mArrayAdapter.getItem(index);
-                           if (eventHash == (databaseObject.getObject().hashCode())) {
-                               searchedDatabaseObject = databaseObject;
-                           }
-                       }
-                       if (searchedDatabaseObject != null) {
-                           mArrayAdapter.remove(searchedDatabaseObject); // not working !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                           mArrayAdapter.insert(searchedDatabaseObject, 0);
-                       }
-                   }
-               }
+               specificEventFromBundleOnTop();
             }
         });
 
@@ -187,5 +169,26 @@ public class SwipeFragment extends Fragment implements SwipeFlingAdapterView.onF
             }
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
+    }
+
+    private void specificEventFromBundleOnTop() {
+        // to have a event on top when clicking on it on the map
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            float eventHash = bundle.getFloat("eventHash", DEFAULT_VALUE);
+            if (eventHash != DEFAULT_VALUE) {
+                DatabaseObject<Event> searchedDatabaseObject = null;
+                for (int index = 0; index < mArrayAdapter.getCount() && searchedDatabaseObject == null; index++) {
+                    DatabaseObject<Event> databaseObject = mArrayAdapter.getItem(index);
+                    if (eventHash == (databaseObject.getObject().hashCode())) {
+                        searchedDatabaseObject = databaseObject;
+                    }
+                }
+                if (searchedDatabaseObject != null) {
+                    mArrayAdapter.remove(searchedDatabaseObject); // not working !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    mArrayAdapter.insert(searchedDatabaseObject, 0);
+                }
+            }
+        }
     }
 }
