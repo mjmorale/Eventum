bf764b7554ac7cb1fbbd43b7dbdfe3324a11f25a
Michel <michel.morales@epfl.ch>
2020-05-12 20:00:33 +0200

    pending work

---
+++ app/src/main/java/ch/epfl/sdp/ui/user/profile/ProfileFragment.java
@@ -0,0 +1,11 @@
+package ch.epfl.sdp.ui.user.profile;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment(){
+        super();
+    }
+}
