312ec1278e5218413bc472e12d3083348cbf24bd
Jonatan Bonjour <jonatan.bonjour@epfl.ch>
2020-05-13 11:06:50 +0200

    new test for SwipeFragment

--- app/src/androidTest/java/ch/epfl/sdp/ui/main/swipe/SwipeFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/ui/main/swipe/SwipeFragmentTest.java
@@ -194,31 +194,30 @@ public class SwipeFragmentTest {
         onView(allOf(withText(eventTest1.getDescription()), isDisplayed())).check(matches(isDisplayed()));
     }
 
-//    @Test
-//    public void SwipeFragment_FromTheMapWithBundleShowTheRightCard() throws InterruptedException {
-//        mockSetup();
-//
-//        Bundle bundle = new Bundle();
-//        bundle.putInt("eventHash", eventTest2.hashCode());
-//
-//        mScenario= FragmentScenario.launchInContainer(
-//                SwipeFragment.class,
-//                bundle,
-//                R.style.Theme_AppCompat,
-//                new MockFragmentFactory(SwipeFragment.class, mDatabase, mAuthenticator, new MockLocationService()));
-//
-//
-//        List<DatabaseObject<Event>> events = new ArrayList<>();
-//        events.add(new DatabaseObject<>(DUMMY_EVENTREF2, eventTest2));
-//        mScenario.onFragment(fragment -> {mEventsLiveData.setValue(events);});
-//
-//        Thread.sleep(1500);
-//
-//        onView(withText("title2")).check(matches(isDisplayed()));
-//        onView(withId(R.id.cards_list_view)).perform(swipeRight());
-//
-//        Thread.sleep(1500);
-//        onView(withId(R.id.mapView)).check(matches((isDisplayed())));
-//    }
+    @Test
+    public void SwipeFragment_FromTheMapWithBundleShowTheRightCard() throws InterruptedException {
+        mockSetup();
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("eventHash", eventTest2.hashCode());
+
+        mScenario= FragmentScenario.launchInContainer(
+                SwipeFragment.class,
+                bundle,
+                R.style.Theme_AppCompat,
+                new MockFragmentFactory(SwipeFragment.class, mDatabase, mAuthenticator, new MockLocationService()));
+
+
+        List<DatabaseObject<Event>> events = new ArrayList<>();
+        events.add(new DatabaseObject<>(DUMMY_EVENTREF2, eventTest2));
+        mScenario.onFragment(fragment -> {mEventsLiveData.setValue(events);});
+
+        Thread.sleep(1500);
+
+        onView(withText("title2")).check(matches(isDisplayed()));
+        onView(withId(R.id.cards_list_view)).perform(click());
+        onView(allOf(withText(eventTest2.getTitle()), isDisplayed())).check(matches(isDisplayed()));
+        onView(allOf(withText(eventTest2.getDescription()), isDisplayed())).check(matches(isDisplayed()));
+    }
 }
 
