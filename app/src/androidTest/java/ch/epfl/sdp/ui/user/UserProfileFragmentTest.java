3b8fdd44f383e3e22e22111d8d03769dc1b4d63b
Karim <karim.sabaa@epfl.ch>
2020-05-20 15:33:00 +0200

    Added tests

--- app/src/androidTest/java/ch/epfl/sdp/ui/user/UserProfileFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/ui/user/UserProfileFragmentTest.java
@@ -31,6 +31,7 @@ import ch.epfl.sdp.ui.user.profile.UserProfileFragment;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
@@ -80,7 +81,7 @@ public class UserProfileFragmentTest {
 
     @SuppressWarnings("unchecked")
     @Test
-    public void UserProfileFragment_test1() {
+    public void UserProfileFragment_test1() throws InterruptedException {
 
 
         FragmentScenario<UserProfileFragment> scenario = FragmentScenario.launchInContainer(
@@ -103,10 +104,14 @@ public class UserProfileFragmentTest {
         intending(hasAction("android.intent.action.PICK")).respondWith(result);
         onView(withId(R.id.user_profile_photo)).perform(click());
         Intents.release();
+        
 
         onView(withId(R.id.user_profile_photo)).check(matches(withTagValue(is("new_image"))));
         onView(withText(mUser.getName())).check(matches(isDisplayed()));
         onView(withText(mUser.getDescription())).check(matches(isDisplayed()));
+
+
+        onView(withId(R.id.user_profile_bio)).perform(typeText(mUser.getDescription()));
     }
 
 }
